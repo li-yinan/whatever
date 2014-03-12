861c6a42d42a974bffe9729a2fe71d8cfb4b49ee
LiYinan <liyinan@LiYinandeMac-mini.local>
2014-03-13 00:40:27 +0800

    whatever

--- app/scripts/controllers/picdetail.js
+++ app/scripts/controllers/picdetail.js
@@ -2,12 +2,19 @@
 
 angular.module('whateverApp')
   .controller('PicdetailCtrl', ["$scope", "Data", "dataUrl", "$routeParams",function ($scope, Data, url, $routeParams) {
+      $scope.showNum = 0;
+      //绑定左右翻页按钮事件
+      var length = 0;
+      $scope.clickLeft = function() {
+          $scope.showNum = ($scope.showNum+length-1)%length;
+      };
+      $scope.clickRight = function() {
+          $scope.showNum = ($scope.showNum+length+1)%length;
+      };
+
+      //获取数据
       Data.get(url.picDetail, {"id":$routeParams.id}, function(data) {
           $scope.data = data;
-          setTimeout(function() {
-              $('#picDetail').carousel({
-                  interval: 2000
-              });
-          }, 0);
+          length = data.length;
       });
   }]);
