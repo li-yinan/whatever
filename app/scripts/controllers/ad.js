149da42b7aaa01fc9dd17bde145f3ab66306149d
LiYinan <liyinan@LiYinandeMac-mini.local>
2014-03-14 00:25:35 +0800

    whatever

--- app/scripts/controllers/ad.js
+++ app/scripts/controllers/ad.js
@@ -2,9 +2,4 @@
 
 angular.module('whateverApp')
   .controller('AdCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
   });
